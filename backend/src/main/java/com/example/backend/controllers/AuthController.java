eccd0f3550caa4e780aa4a9a6d265759f89b7c7a
Alonso Sanhueza <alonso.sanhueza32@gmail.com>
2024-11-13 22:13:59 -0300

    feat(backend): implement JWT-based restrictions and validators

--- backend/src/main/java/com/example/backend/controllers/AuthController.java
+++ backend/src/main/java/com/example/backend/controllers/AuthController.java
@@ -1,8 +1,10 @@
 package com.example.backend.controllers;
 
 import com.example.backend.dtos.LoginDTO;
+import com.example.backend.entities.UserEntity;
 import com.example.backend.services.AuthService;
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -34,6 +36,11 @@ public class AuthController {
         return new ResponseEntity<>(message, HttpStatus.OK);
     }
 
+    @PostMapping("/register")
+    ResponseEntity<UserEntity> register(@RequestBody UserEntity user) {
+        return new ResponseEntity<>(authService.register(user), HttpStatus.CREATED);
+    }
+
     @PostMapping("/logout")
     public ResponseEntity<Map<String, Boolean>> login(HttpServletResponse response){
         Cookie jwtCookie = new Cookie("JWT", null);
@@ -44,6 +51,15 @@ public class AuthController {
 
         response.addCookie(jwtCookie);
 
+        HashMap<String, Boolean> message = new HashMap<>();
+        message.put("success", true);
+        return new ResponseEntity<>(message, HttpStatus.OK);
+    }
+
+    @PostMapping("/verify-token")
+    public ResponseEntity<Map<String, Boolean>> verifyToken(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        authService.verifyToken(cookies);
         HashMap<String, Boolean> message = new HashMap<>();
         message.put("success", true);
         return new ResponseEntity<>(message, HttpStatus.OK);
